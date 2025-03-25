22dea5334d7e6c535c175f75f2d00668b4ba3b24
zhangmiao.zhang <mzhang27@trip.com>
2025-03-25 19:07:33 +0800

    api mock by https://app.beeceptor.com/console/lidomock

--- api/query.js
+++ api/query.js
@@ -1,40 +1,42 @@
 export function userLogin(data) {
-	const immediatePromise = new Promise((resolve) => {
-		const jsonObject = {
-			data: {
-				tokenHead: 'TOKENHEAD',
-				token: 'TOKEN'
-			}
-		};
-		resolve(jsonObject);
-	});
+	// const immediatePromise = new Promise((resolve) => {
+	// 	const jsonObject = {
+	// 		data: {
+	// 			tokenHead: 'TOKENHEAD',
+	// 			token: 'TOKEN'
+	// 		}
+	// 	};
+	// 	resolve(jsonObject);
+	// });
 
-	return immediatePromise;
-	// return request({
-	// 	method: 'POST',
-	// 	url: '/sso/login',
-	// 	header: {
-	// 		'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
-	// 	},
-	// 	data: data
-	// })
+	// return immediatePromise;
+	return uni.request({
+	    url: 'https://lidomock.free.beeceptor.com/login',
+	    data: {},
+	    header: {
+	        'custom-header': 'hello'
+	    }
+	});
 }
 
 export function getUserInfo() {
-	const immediatePromise = new Promise((resolve) => {
-		const jsonObject = {
-			data: {
-				name: '张淼'
-			}
-		};
-		resolve(jsonObject);
-	});
+	// const immediatePromise = new Promise((resolve) => {
+	// 	const jsonObject = {
+	// 		data: {
+	// 			name: '张淼'
+	// 		}
+	// 	};
+	// 	resolve(jsonObject);
+	// });
 
-	return immediatePromise;
-	// return request({
-	// 	method: 'GET',
-	// 	url: '/sso/info'
-	// })
+	// return immediatePromise;
+	return uni.request({
+	    url: 'https://lidomock.free.beeceptor.com/userInfo',
+	    data: {},
+	    header: {
+	        'custom-header': 'hello'
+	    }
+	});
 }
 
 export function getMenu() {
