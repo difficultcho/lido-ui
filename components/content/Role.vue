17505f29028f5c39ece97bb2fe06ee061a9a6731
zhangmiao.zhang <mzhang27@trip.com>
2025-04-10 14:49:37 +0800

    使用对话框

--- components/content/Role.vue
+++ components/content/Role.vue
@@ -1,27 +1,61 @@
 <template>
 	<view>
-		<el-form :model="form">
-			<el-form-item label="姓名" prop="name">
-				<el-input v-model="form.name"></el-input>
-			</el-form-item>
-			<el-form-item label="年龄" prop="age">
-				<el-input v-model="form.age"></el-input>
-			</el-form-item>
-			<el-form-item>
-				<el-button type="primary" @click="onSubmit">提交</el-button>
-			</el-form-item>
-		</el-form>
+		<!-- 搜索区域 -->
+		<div class="operation-bar">
+			<el-input v-model="searchText" placeholder="请输入姓名搜索" clearable style="width: 300px; margin-right: 20px;"
+				@input="handleSearch" />
+			<el-button type="primary" @click="openDialog('add')">新增人员</el-button>
+		</div>
 
-		<el-table :data="tableData">
+		<!-- 表单对话框 -->
+		<el-dialog :title="dialogType === 'add' ? '新增人员' : '编辑人员'" v-model="dialogVisible" width="30%">
+			<el-form :model="form">
+				<el-form-item label="姓名" prop="name">
+					<el-input v-model="form.name"></el-input>
+				</el-form-item>
+				<el-form-item label="年龄" prop="age">
+					<el-input v-model="form.age"></el-input>
+				</el-form-item>
+			</el-form>
+			<template #footer>
+				<el-button @click="dialogVisible = false">取消</el-button>
+				<el-button type="primary" @click="confirmForm">确认</el-button>
+			</template>
+		</el-dialog>
+
+		<!-- 详情对话框 -->
+		<el-dialog title="详细信息" v-model="detailDialogVisible" width="30%">
+			<el-descriptions :column="1" border>
+				<el-descriptions-item label="姓名">{{ currentDetail.name }}</el-descriptions-item>
+				<el-descriptions-item label="年龄">{{ currentDetail.age }}</el-descriptions-item>
+			</el-descriptions>
+		</el-dialog>
+
+		<!-- 删除确认对话框 -->
+		<el-dialog title="提示" v-model="deleteDialogVisible" width="30%">
+			<span>确认删除该记录吗？</span>
+			<template #footer>
+				<el-button @click="deleteDialogVisible = false">取消</el-button>
+				<el-button type="danger" @click="confirmDelete">确认</el-button>
+			</template>
+		</el-dialog>
+
+		<!-- 表格 -->
+		<el-table :data="paginatedData" style="width: 100%">
 			<el-table-column prop="name" label="姓名"></el-table-column>
 			<el-table-column prop="age" label="年龄"></el-table-column>
-			<el-table-column label="操作">
+			<el-table-column label="操作" width="200">
 				<template #default="scope">
-					<el-button @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-					<el-button @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+					<el-button size="small" @click="handleDetail(scope.row)">详情</el-button>
+					<el-button size="small" @click="handleEdit(scope.row)">编辑</el-button>
+					<el-button size="small" type="danger" @click="handleDelete(scope.$index)">删除</el-button>
 				</template>
 			</el-table-column>
 		</el-table>
+
+		<!-- 分页 -->
+		<el-pagination style="margin-top: 20px;" background layout="total, prev, pager, next"
+			:total="filteredData.length" :page-size="pageSize" v-model:current-page="currentPage" />
 	</view>
 </template>
 
@@ -41,30 +75,92 @@
 						name: '李四',
 						age: 25
 					}
-				]
+				],
+				dialogVisible: false,
+				deleteDialogVisible: false,
+				detailDialogVisible: false,
+				dialogType: 'add',
+				currentDetail: {},
+				deleteIndex: -1,
+				searchText: '',
+				currentPage: 1,
+				pageSize: 5
 			};
 		},
-		methods: {
-			onSubmit() {
-				this.tableData.push({
-					...this.form
-				});
-				this.form.name = '';
-				this.form.age = '';
+		computed: {
+			filteredData() {
+				return this.tableData.filter(item =>
+					item.name.toLowerCase().includes(this.searchText.toLowerCase())
+				);
 			},
-			handleEdit(index, row) {
+			paginatedData() {
+				const start = (this.currentPage - 1) * this.pageSize;
+				const end = start + this.pageSize;
+				return this.filteredData.slice(start, end);
+			}
+		},
+		methods: {
+			handleSearch() {
+				this.currentPage = 1; // 搜索后重置到第一页
+			},
+			openDialog(type) {
+				this.dialogType = type;
+				this.dialogVisible = true;
+			},
+			confirmForm() {
+				if (this.dialogType === 'add') {
+					this.tableData.push({
+						...this.form
+					});
+				} else {
+					// 编辑时直接替换原数据
+					const index = this.tableData.findIndex(
+						item => item.name === this.form.name && item.age === this.form.age
+					);
+					if (index > -1) this.tableData.splice(index, 1, {
+						...this.form
+					});
+				}
+				this.resetForm();
+				this.dialogVisible = false;
+			},
+			handleEdit(row) {
 				this.form = {
 					...row
 				};
-				this.tableData.splice(index, 1);
+				this.openDialog('edit');
 			},
-			handleDelete(index, row) {
-				this.tableData.splice(index, 1);
+			handleDelete(index) {
+				this.deleteIndex = index;
+				this.deleteDialogVisible = true;
+			},
+			confirmDelete() {
+				this.tableData.splice(this.deleteIndex, 1);
+				this.deleteDialogVisible = false;
+			},
+			handleDetail(row) {
+				this.currentDetail = {
+					...row
+				};
+				this.detailDialogVisible = true;
+			},
+			resetForm() {
+				this.form = {
+					name: '',
+					age: ''
+				};
 			}
 		}
 	};
 </script>
 
 <style>
-	/* Add any custom styles here */
+	/* 可根据需要添加自定义样式 */
+	.el-dialog__body {
+		padding: 20px;
+	}
+
+	.el-descriptions__label {
+		width: 80px;
+	}
 </style>
