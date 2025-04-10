dcc7217666cb97c5c256db5cee531e95eeac0919
zhangmiao.zhang <mzhang27@trip.com>
2025-04-10 14:23:57 +0800

    汉化

--- components/content/Role.vue
+++ components/content/Role.vue
@@ -1,67 +1,70 @@
 <template>
-  <div>
-    <el-form :model="form">
-      <el-form-item label="Name" prop="name">
-        <el-input v-model="form.name"></el-input>
-      </el-form-item>
-      <el-form-item label="Age" prop="age">
-        <el-input v-model="form.age"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">Submit</el-button>
-      </el-form-item>
-    </el-form>
+	<view>
+		<el-form :model="form">
+			<el-form-item label="姓名" prop="name">
+				<el-input v-model="form.name"></el-input>
+			</el-form-item>
+			<el-form-item label="年龄" prop="age">
+				<el-input v-model="form.age"></el-input>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="primary" @click="onSubmit">提交</el-button>
+			</el-form-item>
+		</el-form>
 
-    <el-table :data="tableData">
-      <el-table-column prop="name" label="Name"></el-table-column>
-      <el-table-column prop="age" label="Age"></el-table-column>
-      <el-table-column label="Operations">
-        <template #default="scope">
-          <el-button @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
-          <el-button @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-  </div>
+		<el-table :data="tableData">
+			<el-table-column prop="name" label="姓名"></el-table-column>
+			<el-table-column prop="age" label="年龄"></el-table-column>
+			<el-table-column label="操作">
+				<template #default="scope">
+					<el-button @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+					<el-button @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+				</template>
+			</el-table-column>
+		</el-table>
+	</view>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      form: {
-        name: '',
-        age: ''
-      },
-      tableData: [
-        {
-          name: 'John Doe',
-          age: 18
-        },
-        {
-          name: 'Jane Smith',
-          age: 25
-        }
-      ]
-    };
-  },
-  methods: {
-    onSubmit() {
-      this.tableData.push({ ...this.form });
-      this.form.name = '';
-      this.form.age = '';
-    },
-    handleEdit(index, row) {
-      this.form = { ...row };
-      this.tableData.splice(index, 1);
-    },
-    handleDelete(index, row) {
-      this.tableData.splice(index, 1);
-    }
-  }
-};
+	export default {
+		data() {
+			return {
+				form: {
+					name: '',
+					age: ''
+				},
+				tableData: [{
+						name: '张三',
+						age: 18
+					},
+					{
+						name: '李四',
+						age: 25
+					}
+				]
+			};
+		},
+		methods: {
+			onSubmit() {
+				this.tableData.push({
+					...this.form
+				});
+				this.form.name = '';
+				this.form.age = '';
+			},
+			handleEdit(index, row) {
+				this.form = {
+					...row
+				};
+				this.tableData.splice(index, 1);
+			},
+			handleDelete(index, row) {
+				this.tableData.splice(index, 1);
+			}
+		}
+	};
 </script>
 
 <style>
-/* Add any custom styles here */
+	/* Add any custom styles here */
 </style>
